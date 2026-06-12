63a815b31758db53d9b123c1ba17ee94fa7143e7
yasminvoliveira <yasmin.v.oliveira6@edu.senai.br>
2026-06-12 08:58:49 -0300

    Adicionei uma nova função para travar o menu

--- NaveEspacial.py
+++ NaveEspacial.py
@@ -11,31 +11,32 @@ def viajar():  ### Viajar irá gastar o combústivel
         print("Não há tripulantes na na nave, adicione um tripulante!")
         print("Para adicionar um tripulante selecione 4")
 
-    elif combustivel>=30:
-        combustivel = combustivel - 30
-        print("* A nave está pronta para a viagem")
-
     else:
-        print("* Combustível insuficiente para a viagem, abasteça!")
+        if combustivel>=30:
+            combustivel = combustivel - 30
+            print("* A nave está pronta para a viagem")
 
+        else:
+            print("* Combustível insuficiente para a viagem, abasteça!")
+    travarMenu()
 
 
 def abastecer():
     global combustivel
     combustivel = 100
     print("* Tanque cheio⛽")
-
+    travarMenu()
 def status_nave():
     print(f"-------- STATUS da NAVE -----------")
     print(f"* Temos {combustivel} de combustível")
     print(f"* Os tripulantes são {tripulantes}")
-
+    travarMenu()
 # Registrando novo tripulante
 def registrarTripulante():
     novoTripulante = input("Inserir novo tripulante : ")
     tripulantes.append(novoTripulante)
     print("Tripulante inserido com sucesso!🚀")
-
+    travarMenu()
 
 ##Criando uma função que tira o ultimo tripulante
 def tirandotripulante():
@@ -46,6 +47,12 @@ def tirandotripulante():
     else:
         print(f"Os tripulantes são: {tripulantes}")
     tripulantes.pop()
+    travarMenu()
+## Criando uma função para pausar o código entre as interações do usuário
+
+def travarMenu():
+    ##o código começa aqui
+    input("\nPressione <ENTER> para continuar....")
 
 
 ## Criando um menu
